da0e2c9810072ba47c9c47d061786a769bbd8509
Stephen Sheridan <stephensheridan@users.noreply.github.com>
2020-11-19 15:06:14 +0000

    Initial commit
    First commit

---
+++ mergeFiles.py
@@ -0,0 +1,38 @@
+# Import the os module, for the os.walk function
+import os
+import csv
+
+def processSample(filePath):
+    # Use a list to store op code counts for sample
+    values = []
+    print('Processing %s' % filePath)
+    with open(filePath) as fp:
+        for line in fp:
+            count = (line.strip().split(',')[1]).strip()
+            values.append(count)
+    # Return the list with op code counts
+    return values
+
+def mergeFile(filePath):
+    # Column headers in same order as sample files
+    columns = ['add','and','call','cmp','eax','ecx','jmp','lea','mov','mul','or','push','pop','test','xor']
+    # Open the master file for writing
+    masterFile = open('./master.csv', 'a')
+    # Create a csv writer
+    write = csv.writer(masterFile)
+    # Output the column headinds - optional!
+    write.writerow(columns)
+    # Traverse the sample files as process
+    for dirName, subdirList, fileList in os.walk(filePath):
+        for fname in fileList:
+            # Get the list of op code counts from each sample file
+            counts = processSample(filePath + '/' + fname)
+            # Write the op code counts as a row in the master file
+            write.writerow(counts)
+    # Close the master file
+    masterFile.close()        
+
+
+if __name__ == '__main__':
+    # Pass over the folder with the sample files
+    mergeFile("./data")
